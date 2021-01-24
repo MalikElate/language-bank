af16cdd438599cfa37e1b631fff05f1082e82551
Malik Elate <Malik.e1955@gmail.com>
2021-01-24 11:28:00 -0600

    updated details page

--- src/components/WorldMap/WorldMap.jsx
+++ src/components/WorldMap/WorldMap.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+// import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+// import MapChart from "./MapChart";
+import WorldMap from 'react-world-map';
 import {
   Grid,
   withStyles,
@@ -13,10 +15,20 @@ const styles = {
 class WorldMapComponent extends Component {
   render() {
     const { classes } = this.props; 
+    const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
     return (
-      <Grid>
-        Hello from WorldMap
-      </Grid>
+      <div>
+      {/* <ComposableMap>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+          }
+        </Geographies>
+      </ComposableMap>
+      asdfasf */}
+      <WorldMap />
+    </div>
     );
   }
 }
